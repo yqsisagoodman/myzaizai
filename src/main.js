68f5ccbb9286fd6ffec021faf48945736ad8d413
yqs <1479666453@qq.com>
2020-08-14 15:18:09 +0800

    新建项目 赤足外卖

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
 
-Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+//创建全局bus
+Vue.prototype.$bus = new Vue();
+
+Vue.config.productionTip = false;
+
 
 new Vue({
+  router,
+ 
   render: h => h(App),
 }).$mount('#app')
